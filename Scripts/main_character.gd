1af5fcefbb5695426fa64950ecb1abbc21291a93
Dante Azucar <Az5320Da0606@pusd.us>
2024-10-28 09:52:55 -0700

    almost done with zombie movement and behaviors

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -1,9 +1,37 @@
 extends CharacterBody2D
 
+@export var speed = 50
+
+
 
 func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+	
 	look_at(get_global_mouse_position())
-	# Add the gravity.
+	if Global.player_noise > 40:
+		Global.player_noise_x = self.position.x
+		Global.player_noise_y = self.position.y
+	else:
+		Global.player_pos_x = self.position.x
+		Global.player_pos_y = self.position.y
+
 	
 
+	
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_sprint = Input.is_action_pressed("Sprint")
+	if input_sprint == true:
+		speed = 100
+		Global.player_noise = 50
+		print(Global.player_noise)
+	else:
+		speed = 50
+		Global.player_noise = 10
+
+	velocity = input_direction * speed
+
+	
 	
--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -1,21 +1,115 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var given_x = self.position.x
+@onready var given_y = self.position.y
+var speed = .5
+var seen_ = false
+var hearing = false
+var timer = 0.0
+var interval = 5.0
 
 func _physics_process(_delta):
-	pass
+	timer += _delta
+	
+	if seen_ == true:
+		print("Self:", self.position.y, " " , self.position.x)
+		look_at(Vector2(given_x,given_y))
+		if self.position.y < given_y:
+			self.position.y += speed
+		else:
+			self.position.y -= speed
+
+		if self.position.x < given_x:
+			self.position.x +=  speed
+		else:
+			self.position.x -= speed
+		seen()
+
+		
+		
+	elif hearing == true:
+		look_at(Vector2(given_x,given_y))
+		
+		if self.position.x < given_x:
+			self.position.x += speed
+		else:
+			self.position.x -= speed
+
+		if self.position.y < given_y:
+			self.position.y += speed
+		else:
+			self.position.y -= speed
+	else:
+		look_at(Vector2(given_x,given_y))
+		
+		if self.position.x < given_x:
+			self.position.x += speed
+		else:
+			self.position.x -= speed
+
+		if self.position.y < given_y:
+			self.position.y += speed
+		else:
+			self.position.y -= speed
+		if timer >= interval:
+			timer = 0
+			random_movement()
+		
+	
+
+	
 
 func investigate():
-	#if body detects noise run go to last noisy x-y cordinate
-	pass
+	
+		given_x = Global.player_noise_x
+		given_y = Global.player_noise_y
+		hearing = true
+	
+	
+	# changes the x and y to be the player x and y
+
+	
 
 func seen():
-	#if in vision cone follow player until out of sight then return to random movement
-	pass
+	
+		given_x = Global.player_pos_x
+		given_y = Global.player_pos_y
+		print("Given", given_x, " ", given_y)
+
+		seen_ = true
+
+	
 
 func random_movement():
-	#randomly go anywhere with no real reason until investiate or seen is proc'd
-	pass
+	print("going in random direction")
+	given_x = randi_range(-100, 100)
+	given_y = randi_range(-100, 100)
+	print(given_x)
+	print(given_y)
+	
+
+func _on_hearing_body_entered(body):
+	if body.has_method("get_input"):
+		if Global.player_noise > 40:
+			print("investigating")
+			investigate()
+
+
+func _on_hearing_body_exited(body):
+	if body.has_method("get_input"):
+		hearing = false
+		random_movement()
+
+
+func _on_vision_cone_body_exited(body):
+	if body.has_method("get_input"):
+		seen_ = false
+		print("bye")
+		random_movement()
+
+
+func _on_vision_cone_body_entered(body):
+	if body.has_method("get_input"):
+		print("hi")
+		seen()
